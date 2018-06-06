5b02bf63eb8ba5a5187fe4a98cb26bac608b01fe
Alex <Alexandrecartier00@gmail.com>
2018-06-06 10:57:30 +0200

    Fixed Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-
   end
 
   def new
@@ -11,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user 
+      log_in @user
       flash[:success] = "Welcome to the Unbiasemy platform!"
       redirect_to @user
     else
